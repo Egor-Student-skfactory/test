4e3f8c0eb929146e7397c2d278fd1895a4b55b28
Egor <kegor5831@gmail.com>
2023-01-26 23:43:38 +0300

    first

---
+++ src1/patern1.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class patern1 {
+    public static void main(String[] args){
+
+        String text =
+            "WrongMsisdnError SuccessfulTransaction TimeOutError InternalError SuccessfulTransaction ExternalIDError SuccessfulTransaction WrongIDError" +
+                    "InvalidArgumentError SuccessfulTransaction SuccessfulTransaction SuccessfulTransaction WrongMsisdnError";
+        String [] str =text.split(" ");
+      for (String s:str){
+          if (Pattern.matches(".*?Error",s)){
+              System.out.println(s +" ");//7 задача
+          }
+        }
+    }
+}
+// Pattern pattern = Pattern.compile("");
+// Matcher matcher = pattern.matcher(text);
+//System.out.println(matcher.group());
